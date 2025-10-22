a2ed0bb706ba39813dfc3a9c5bd3490e5d9463d2
Luiz Henrique <luiz.oliveirabarbosa@eaportal.org>
2025-10-22 17:59:50 -0300

    atividade 16/10

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,22 @@ const port = 3333
 //GET, POST, PATCH, PUT, DELETE
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(persons)
 })
 
+app.post("/cadastrar", (request, response) => {
+    //desestruturação
+    const {user} = request.body
+    console.log(user)
+
+    //cadastro no banco de dados
+
+    response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+})
+
 app.listen(port, () => {
-    console.log(`Server run on port ${port}!`)
+    console.log(`Server running on port ${port}!`)
 })
