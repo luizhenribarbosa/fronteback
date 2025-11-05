44bdf03c9427884a02bfd84722c714b319cfc078
Luiz Henrique <luiz.oliveirabarbosa@eaportal.org>
2025-11-05 20:43:31 -0300

    atividade 16/10

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from "cors"
-
+import mysql2 from "mysql2"
 import { persons } from "./persons.js"
+
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
 
 const app = express() 
 const port = 3333
@@ -21,10 +23,32 @@ app.post("/cadastrar", (request, response) => {
     console.log(user)
 
     //cadastro no banco de dados
+    const insertCommand = `
+        INSERT INTO luizbarbosa_02mb(name, email, password)
+        VALUES (?,?, ?)
+    `
 
-    response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+    database.query(insertCommand, [user.name, user.email, user.password], (error) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+    })
+
+    
 })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}!`)
+})
+
+const database = mysql2.createPool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    connectionLimit: 10
+
 })
